510f2182059296d4c718fce7bd90896a690682f6
Chris Hand <cj.hand@gmail.com>
2022-09-06 22:26:52 -0700

    My Version of the solution

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let diceNames = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+    
+    @IBOutlet weak var diceOne: UIImageView!
+    @IBOutlet weak var diceTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollClicked(_ sender: Any) {
+        let one = Int.random(in: 0...5)
+        let two = Int.random(in: 0...5)
+        diceOne.image = UIImage(named: diceNames[one])
+        diceTwo.image = UIImage(named: diceNames[two])
+    }
 }
-
